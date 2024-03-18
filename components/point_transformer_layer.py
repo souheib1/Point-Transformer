fix vanishing gradient issues during training

components/point_transformer_layer.py
@@ -35,8 +35,12 @@ class PointTransformerLayer(nn.Module):
         k (int, optional): Number of nearest neighbors to consider. Defaults to 16.
         """  
         super().__init__()
-        self.k = k 
+        self.k = k # number of neighbors
         self.linear1 = nn.Linear(dim_in, dim_out) # (d->f)
+        self.linear2 = nn.Linear(dim_out, dim_in) # (f->d)
+        self.keys = nn.Linear(dim_out, dim_out, bias=False) # psi (f->f)
+        self.queries = nn.Linear(dim_out, dim_out, bias=False) #phi (f->f)
+        self.values= nn.Linear(dim_out, dim_out, bias=False) # alpha (f->f)
         self.mapping = nn.Sequential(   #gamma (f->f)
                                     nn.Linear(dim_out, dim_out), 
                                     nn.ReLU(), 
@@ -47,14 +51,10 @@ class PointTransformerLayer(nn.Module):
                                     nn.ReLU(), 
                                     nn.Linear(dim_out, dim_out)
                                     )
-        self.keys = nn.Linear(dim_out, dim_out, bias=False) # psi (f->f)
-        self.queries = nn.Linear(dim_out, dim_out, bias=False) #phi (f->f)
-        self.values= nn.Linear(dim_out, dim_out, bias=False) # alpha (f->f)
-        self.linear2 = nn.Linear(dim_out, dim_in) # (f->d)
 
     def forward(self, coordinates, features):
         
-        # Local Attetion 
+        # Local Attetion : coordinates of nearest neighbors
         dists = torch.sum((coordinates[:, :, None] - coordinates[:, None]) ** 2, dim=-1)  # Compute pairwise squared euclidean distance 
         knn_idx = dists.argsort()[:, :, :self.k]  # Get indices of nearest neighbors 
         knn_coords = points_from_idx(coordinates, knn_idx)  # Gather coordinates of nearest neighbors 
@@ -62,16 +62,16 @@ class PointTransformerLayer(nn.Module):
         
         before = features
         x = self.linear1(features) 
+        
         # Compute queries, keys, and values
         q = self.queries(x)  
         k = points_from_idx(self.keys(x), knn_idx)  
         v = points_from_idx(self.values(x), knn_idx) 
-
         # Compute positional encoding
         pos_enc = self.positional_encoding(coordinates[:, :, None] - knn_coords)  
 
         # Compute attention
-        attention = self.mapping(q[:, :, None] - k + pos_enc) 
+        attention = self.mapping(q[:, :, None] - k + pos_enc) # compute raw attetion
         attention = torch.nn.functional.softmax(attention / np.sqrt(k.size(-1)), dim=-2) # division leads to more stable gradients.
         #print(attention.shape)
         out = torch.einsum('bmnf,bmnf->bmf', attention, v + pos_enc)  # apply attention to values 
@@ -85,14 +85,16 @@ class PointTransformerLayer(nn.Module):
 if __name__ == '__main__':
     
     print("Point Transformer Layer")
-    layer = PointTransformerLayer(dim_in=128, dim_out=3, k=16)
-    B, N, dim_in = 16, 1024, 128
-    print("\t batch_size=",B)
-    print("\t dim_in=",dim_in)
-    print("\t N_points=",N)
-    print("\t Local_Neighborhood k=",16)
-    coords = torch.rand((B, N, 3))
-    features = torch.rand((B, N, dim_in))
+    layer = PointTransformerLayer(dim_in=10, dim_out=3, k=16)
+    batch_size = 16
+    npoints = 1024 
+    dim_in = 10
+    coords = torch.rand((batch_size, npoints, 3))
+    features = torch.rand((batch_size, npoints, dim_in))
     output, attention = layer(coords, features)
+    print("\t batch_size=",batch_size)
+    print("\t dim_in=",dim_in)
+    print("\t N_points=",npoints)
+    print("\t Local_Neighborhood k=",16)
     print('New feature shape:', output.shape)
-    print('Attention shape:', attention.shape)
+

components/transition_layers.py
@@ -9,11 +9,26 @@
 """
 import torch 
 import torch.nn as nn
-import torch.nn.functional as F
-import numpy as np
 
 
-def farthest_point_sample(points, npoint):
+def maxPooling(features):
+    global_feature, _ = torch.max(features, 2)
+    return global_feature
+
+def meanPooling(features):
+    global_feature = torch.mean(features, 2)
+    return global_feature
+
+def points_from_idx(points, idx):
+    """
+    gather point coordianates from a set using indices.
+    """
+    raw_size = idx.size()
+    idx = idx.reshape(raw_size[0], -1)
+    res = torch.gather(points, 1, idx[..., None].expand(-1, -1, points.size(-1)))
+    return res.reshape(*raw_size, -1)
+
+def farthest_point_sample(points, npoint): # From PointNetV2
     """
     Identify a well-spread subset P2 ⊂ P1 with the requisite cardinality
     Input:
@@ -35,17 +50,8 @@ def farthest_point_sample(points, npoint):
     return centroids.to(device)
 
 
-def points_from_idx(points, idx):
-    """
-    gather point coordianates from a set using indices.
-    """
-    raw_size = idx.size()
-    idx = idx.reshape(raw_size[0], -1)
-    res = torch.gather(points, 1, idx[..., None].expand(-1, -1, points.size(-1)))
-    return res.reshape(*raw_size, -1)
-
 class TransitionDownLayer(nn.Module):
-    def __init__(self, npoint, k, input_dim, output_dim):
+    def __init__(self, npoint, k, input_dim, output_dim, pooling='max'):
         """
         Transition Down Layer 
         Input :
@@ -57,6 +63,7 @@ class TransitionDownLayer(nn.Module):
         super().__init__()
         self.npoint = npoint
         self.k = k
+        self.pooling = pooling
         
         # MLP layers for processing point features 
         self.mlp_convs = nn.Sequential(
@@ -74,18 +81,18 @@ class TransitionDownLayer(nn.Module):
         # Farthest point sampling
         fps_idx = farthest_point_sample(xyz, self.npoint) 
         new_xyz = points_from_idx(xyz, fps_idx) 
-        torch.cuda.empty_cache()
-        # Find k nearest neighbors
         dists = torch.sum(((new_xyz[:, :, None] - xyz[:, None]) ** 2), dim=-1)
-        idx = dists.argsort()[:, :, :self.k]  
+        idx = dists.argsort()[:, :, :self.k]  # Find k nearest neighbors
         torch.cuda.empty_cache()
         
-        # Extract features for each point and perform max pooling
-        new_features = features.transpose(1,2) 
-        new_features = self.mlp_convs(new_features) 
-        new_features = new_features.transpose(1,2) 
+        # Extract features for each point 
+        new_features = self.mlp_convs(features.transpose(1,2)).transpose(1,2) 
         grouped_features = points_from_idx(new_features, idx) 
-        new_features, _ = torch.max(grouped_features, 2) 
+        # Perform pooling
+        if self.pooling == 'mean':
+            new_features = meanPooling(grouped_features)
+        else : 
+            new_features = maxPooling(grouped_features)
         return new_xyz, new_features
 
 