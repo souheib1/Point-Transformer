3db2a9593251c0bf414a3d8f8cc51eefaf82f8e0
Souheib Ben Mabrouk <souheib.benmabrouk@supcom.tn>
2024-03-18 12:11:19 +0100

    add models

--- model.py
+++ model.py
@@ -7,13 +7,14 @@ from components.transition_layers import TransitionDownLayer
 
 
 class PointTransformerModel(nn.Module):
-    def __init__(self, npoints=1024, nblocks=4, k=16, num_class=40, input_dim=6):
+    def __init__(self, npoints=1024, nblocks=4, k=16, num_class=40, input_dim=6,pooling='max'):
         super().__init__()
-        
+        self.nblocks = nblocks
+        self.pooling = pooling
         self.MLP1 = nn.Sequential(
-            nn.Linear(input_dim, 16),
+            nn.Linear(input_dim, 32),
             nn.ReLU(),
-            nn.Linear(16, 32)
+            nn.Linear(32, 32)
         )
         
         self.transformer_initial = PointTransformerLayer(32, 64, k)
@@ -21,27 +22,26 @@ class PointTransformerModel(nn.Module):
         self.down_blocks = nn.ModuleList([TransitionDownLayer(npoints // 4 ** i, 
                                                               k, 
                                                               32 * 2 ** i // 2, 
-                                                              32 * 2 ** i) 
-                                        for i in range(1, nblocks + 1)])
+                                                              32 * 2 ** i,
+                                                              pooling=self.pooling) for i in range(1, nblocks + 1)])
         
         self.transformer_blocks = nn.ModuleList([PointTransformerLayer(32 * 2 ** i, 
                                                                        64, 
-                                                                       k) 
-                                                for i in range(1, nblocks + 1)])
+                                                                       k) for i in range(1, nblocks + 1)])
         
         self.MLP2 = nn.Sequential(
             nn.Linear(32*2**nblocks, 256),
             nn.ReLU(),
-            nn.Linear(256, 128),
+            nn.Linear(256, 64),
             nn.ReLU(),
-            nn.Linear(128, num_class)
+            nn.Linear(64, num_class)
         )
     
     def forward(self, x):
         xyz = x[..., :3]
         features,_ = self.transformer_initial(xyz,self.MLP1(x))
 
-        for i in range(len(self.transformer_blocks)):
+        for i in range(self.nblocks):
             xyz, features = self.down_blocks[i](xyz, features)
             features,_ = self.transformer_blocks[i](xyz, features)
         
@@ -50,6 +50,7 @@ class PointTransformerModel(nn.Module):
     
 if __name__ == '__main__':
     model = PointTransformerModel()
+    print(model)
     batch_size, N, input_dim = 16, 1024, 6
     features = torch.rand((batch_size, N, input_dim))
     output = model(features)
